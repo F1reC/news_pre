f1c562603e805e8747bfcf1a6bff86aa39714484
陈增耀 <2692830800@qq.com>
2023-06-10 10:25:09 +0800

    czy-更新注释

--- pages/subscibe/article.js
+++ pages/subscibe/article.js
@@ -1,30 +1,33 @@
+// 引入两个模块，getSubscribeChannels获取用户订阅的频道，getNewsByCategory获取某个类别的新闻
 const { getSubscribeChannels, getNewsByCategory } = require("../../utils/api")
 
 Page({
     data: {
-        channelId: 0,
-        channel: {},
-        articles: []
+        channelId: 0, // 频道ID，默认为0
+        channel: {}, // 频道信息，默认为空对象
+        articles: [] // 文章列表，默认为空数组
     },
-    onShow(){
+    onShow(){  // 页面显示/切入前台时触发
         
     },
-    onLoad: function({ channel }) {
-        this.setData({ channelId: channel })
-        this.getChannelInfo()
-        this.getNewsInfo()
+    onLoad: function({ channel }) { // 页面加载时运行，接受传入的channel参数
+        this.setData({ channelId: channel }) // 设置当前页面的channelId数据为传入的channel
+        this.getChannelInfo() // 获取并设置当前频道的详细信息
+        this.getNewsInfo() // 获取并设置当前频道的新闻信息
     },
 
+    // 获取并设置当前频道的详细信息的异步方法
     async getChannelInfo () {
-        const { channelId } = this.data
-        const channels = await getSubscribeChannels()
-        const channel = channels.find(c => c.id == channelId)
-        this.setData({ channel })
-        wx.setNavigationBarTitle({ title: channel.name, })
+        const { channelId } = this.data // 获取当前频道ID
+        const channels = await getSubscribeChannels() // 获取用户订阅的所有频道
+        const channel = channels.find(c => c.id == channelId) // 在所有订阅频道中找到当前频道
+        this.setData({ channel }) // 更新当前频道信息
+        wx.setNavigationBarTitle({ title: channel.name, }) // 将小程序的导航栏标题设置为当前频道的名字
     },
 
+    // 获取并设置当前频道的新闻信息的异步方法
     async getNewsInfo () {
-        const articles = await getNewsByCategory(this.data.channelId, 1)
-        this.setData({ articles })
+        const articles = await getNewsByCategory(this.data.channelId, 1) // 获取当前频道的新闻，传入参数为频道ID和页码（这里是第一页）
+        this.setData({ articles }) // 更新文章列表
     }
 })
--- utils/api.js
+++ utils/api.js
@@ -1,33 +1,39 @@
 // API DOMAIN
+// 这里设定了两个可能的域名，一个是本地的，一个是线上的
 // JSON 文件见 mock 文件夹
 // const DOMAIN = 'http://localhost:8080/mock'
 const DOMAIN = 'https://h5.ahmq.net/demo/miniapp-static-api/mock'
 
+// 定义一个通用的get请求函数
 export function get (url, data){
   return request(url, 'GET', data)
 }
 
+// 定义一个通用的post请求函数
 export function post (url, data){
   return request(url, 'POST', data)
 }
 
 // 获取全部分类列表
+// 首先从服务器获取所有的分类，然后再获取用户已经关注的分类
+// 最后返回一个新的列表，其中包含用户是否已经关注每一个分类的信息
 export async function getCategroyList () {
-  const categorys = await get('api-category-list.json')
-  const subscribeList = await getSubscribeList()
+  const categorys = await get('api-category-list.json') // 从服务器获取所有的分类
+  const subscribeList = await getSubscribeList() // 获取用户已经关注的分类
   const maped = Array.isArray(subscribeList) ? subscribeList.reduce((prev, k) => {
     prev[k] = true
     return prev
   }, {}) : null
   return categorys.map(category => {
-    // 未设置过关注
-    category.selected = maped ? (!!maped[category.id]) : true
+    category.selected = maped ? (!!maped[category.id]) : true // 如果用户已经关注了这个分类，那么在返回的列表中标记为已关注
     return category
   })
 }
 
+// 小程序的本地存储的key，用于存储用户关注的分类
 const $SubscribeListCacheKey = '$SubscribeList'
-// 读取关注列表
+
+// 获取用户关注的分类列表
 export function getSubscribeList () {
   return new Promise(resolve => {
     wx.getStorage({
@@ -38,7 +44,7 @@ export function getSubscribeList () {
   })
 }
 
-// 保存关注列表
+// 保存用户关注的分类列表
 export function saveSubscribeList (subscribeList) {
   return new Promise((resolve, reject) => {
     wx.setStorage({
@@ -50,29 +56,35 @@ export function saveSubscribeList (subscribeList) {
   })
 }
 
-// 加载轮播图
+// 获取首页的轮播图
 export async function getHomeBannerList () {
   return await get('api-banners.json')
 }
 
-// 根据分类和页码加载数据：模拟的数据重新打乱
+// 根据分类和页码加载数据
+// 模拟的数据重新打乱
 export async function getNewsByCategory (categoryId, pageId = 1) {
   const data = await get(`api-news-list.json`, { categoryId, pageId })
-  return data.sort((a, b) => 0.5 - Math.random())
+  return data.sort((a, b) => 0.5 - Math.random()) // 随机打乱获取的新闻列表
 }
 
-// 订阅频道列表
+// 获取用户订阅的频道列表
 export async function getSubscribeChannels () {
   return await get('api-subscibe-list.json')
 }
 
-// 获取文章详情
+// 获取文章的详细信息
 export async function getArticleDetail (articleId) {
   return await get('api-news-detail.json', { articleId })
 }
 
+// 定义一个通用的请求函数
+// 这个函数接受三个参数：请求的url，请求的方法，以及请求的数据
+// 这个函数会先显示一个加载条，然后发送请求
+// 请求成功后，会隐藏加载条，然后判断请求的结果是否成功
+// 如果成功，那么返回请求的数据，否则抛出一个错误
 export function request(api, method, data = {}){
-  wx.showNavigationBarLoading()
+  wx.showNavigationBarLoading() // 显示加载条
   return new Promise((resove, reject) => {
     wx.request({
       url: `${DOMAIN}/${api}`,
@@ -80,17 +92,17 @@ export function request(api, method, data = {}){
       method: method,
       dataType: 'json',
       success: function(res){
-        wx.hideNavigationBarLoading()
+        wx.hideNavigationBarLoading() // 隐藏加载条
         const data = res.data
         if (data.code) {
-          reject(new Error(data.message))
+          reject(new Error(data.message)) // 如果服务器返回的code不为0，那么说明请求失败，抛出一个错误
         } else {
-          resove(data.data)
+          resove(data.data) // 如果服务器返回的code为0，那么说明请求成功，返回请求的数据
         }
       },
       fail: function(err) {
-        wx.hideNavigationBarLoading()
-        reject(new Error(err.errMsg))
+        wx.hideNavigationBarLoading() // 隐藏加载条
+        reject(new Error(err.errMsg)) // 如果请求失败，那么抛出一个错误
       }
     })
   })
