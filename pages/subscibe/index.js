8844efa83fca1190ee6f53a38564cd6b3a4f2eef
asuka1733 <994082122@qq.com>
2023-06-11 18:26:54 +0800

    更新注释
    更新注释

--- pages/subscibe/index.js
+++ pages/subscibe/index.js
@@ -8,7 +8,7 @@ Page({
     // 页面初始化 options为页面跳转所带来的参数
     this.updateSubscribeChannels()
   },
-
+// 获取并设置当前频道的详细信息的异步方法
   async updateSubscribeChannels () {
     const channels = await getSubscribeChannels()
     this.setData({ channels })
